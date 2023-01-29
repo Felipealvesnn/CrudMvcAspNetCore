2bac9e8c240256df0a242a1c83d31ea19acf8b44
Felipealvesnn <felipe.alvesnn@gmail.com>
2023-01-29 12:25:10 -0300

    att

--- MacorattiMVC/MacorratiMVC.Infra.data/EntitiesConfiguration/CategoriesConfiguration.cs
+++ MacorattiMVC/MacorratiMVC.Infra.data/EntitiesConfiguration/CategoriesConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using MacorattiMVC.Domain.Entitites;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MacorratiMVC.Infra.data.EntitiesConfiguration
 {
